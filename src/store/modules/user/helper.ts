29e98890f40e0b01502be31d45920f829cdd824b
prinyi <87046964@qq.com>
2023-04-01 17:09:07 +0800

    1

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      avatar: 'https://raw.githubusercontent.com/mynew/chatgpt-web/main/src/assets/avatar.jpg',
+      name: 'chatgpt',
+      description: 'Star on <a href="https://chat.openai.com/" class="text-blue-500" target="_blank" >Openai</a>',
     },
   }
 }
--- src/views/chat/layout/sider/index.vue
+++ src/views/chat/layout/sider/index.vue
@@ -73,7 +73,7 @@ watch(
       <main class="flex flex-col flex-1 min-h-0">
         <div class="p-4">
           <NButton dashed block @click="handleAdd">
-            {{ $t('chat.newChatButton') }}
+            <strong>{{ $t('chat.newChatButton') }}🔊</strong>
           </NButton>
         </div>
         <div class="flex-1 min-h-0 pb-4 overflow-hidden">
@@ -81,7 +81,7 @@ watch(
         </div>
         <div class="p-4">
           <NButton block @click="show = true">
-            {{ $t('store.siderButton') }}
+            <strong>{{ $t('store.siderButton') }}🔊</strong>
           </NButton>
         </div>
       </main>
